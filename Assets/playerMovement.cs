721db5a6d8931e933b8a462d85ef9f0501f01a96
Magnus Mølgaard Christensen <magnuschristensen42@gmail.com>
2022-10-04 14:46:13 +0200

    added better jump, and deleted former code

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    private float walkSpeed = 6f;
+    private float walkSpeed = 6.0f;
     private float moveInput;
     private Rigidbody2D rb;
     public float jumpValue = 0.0f;
@@ -26,15 +26,23 @@ public class playerMovement : MonoBehaviour
     {
         moveInput = Input.GetAxisRaw("Horizontal");
 
-        rb.velocity = new Vector2(moveInput * walkSpeed, rb.velocity.y);
+        if (jumpValue == 0.0f && isGrounded)
+        {
+            rb.velocity = new Vector2(moveInput * walkSpeed, rb.velocity.y);
+        }
         
         isGrounded = Physics2D.OverlapBox(
             new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.5f), 
             new Vector2(0.9f, 0.4f), 0f, groundMask);
+
+        if (Input.GetKeyDown("space") && isGrounded && canJump)
+        {
+            rb.velocity = new Vector2(0.0f, rb.velocity.y);
+        }
         
         if (Input.GetKey("space") && isGrounded && canJump)
         {
-            jumpValue += 0.1f;
+            jumpValue += 0.05f;
         }
         
         if (jumpValue >= 20f && isGrounded)
@@ -42,15 +50,23 @@ public class playerMovement : MonoBehaviour
             float tempx = moveInput * walkSpeed;
             float tempy = jumpValue;
             rb.velocity = new Vector2(tempx, tempy);
+            Invoke("ResetJump", 0.2f);
         }
-        
+
+        if (Input.GetKeyUp("space"))
+        {
+            if (isGrounded)
+            {
+                rb.velocity = new Vector2(moveInput * walkSpeed, jumpValue);
+                jumpValue = 0.0f;
+            }
+            canJump = true;
+        }
     }
 
-    /*
-    private void FixedUpdate()
+    void ResetJump()
     {
-        rb.velocity = new Vector2(moveInput * walkSpeed, rb.velocity.y);
+        canJump = false;
+        jumpValue = 0;
     }
-    */
-    
 }
