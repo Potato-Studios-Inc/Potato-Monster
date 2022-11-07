4d22e7cc746ea5114e4a44e23a0affd73180bb24
Lasse Hyldahl Jensen <lhj@lunar.app>
2022-11-07 10:27:09 +0100

    Implement player sprite with animations (#5)
    * Implement walk, aiming to jump and idle animations
    
    * Refactor player movement and add jump animation

--- Assets/Player/playerMovement.cs
+++ Assets/Player/playerMovement.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    private float walkSpeed = 6.0f;
-    private float moveInput;
+    private float walkSpeed = 2.0f;
     private Rigidbody2D rb;
     public float jumpValue = 0.0f;
 
@@ -15,23 +14,28 @@ public class playerMovement : MonoBehaviour
     
     public bool canJump = true;
     public bool isGrounded;
+    private Animator _animator;
+    private bool _isAimingToJump = false;
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+    private static readonly int IsAimingToJump = Animator.StringToHash("IsAimingToJump");
 
 
     private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveInput = Input.GetAxisRaw("Horizontal");
+        var inputX = Input.GetAxisRaw("Horizontal");
 
         rb.sharedMaterial = jumpValue > 0 ? bounceMat : normalMat;
         
         if (jumpValue == 0.0f && isGrounded)
         {
-            rb.velocity = new Vector2(moveInput * walkSpeed, rb.velocity.y);
+            rb.velocity = new Vector2(inputX * walkSpeed, rb.velocity.y);
         }
 
         isGrounded = Physics2D.OverlapBox(
@@ -40,36 +44,42 @@ public class playerMovement : MonoBehaviour
 
         if (Input.GetKeyDown("space") && isGrounded && canJump)
         {
-            rb.velocity = new Vector2(0.0f, rb.velocity.y);
+            OnStartAimingToJump();
         }
 
         if (Input.GetKey("space") && isGrounded && canJump)
         {
-            jumpValue += 0.02f;
-        }
-        
-        if (jumpValue >= 15f && isGrounded)
-        {
-            float tempx = moveInput * walkSpeed;
-            float tempy = jumpValue;
-            rb.velocity = new Vector2(tempx, tempy);
-            Invoke("ResetJump", 0.1f);
+            OnAimingToJump();
         }
 
-        if (Input.GetKeyUp("space"))
+        if (jumpValue >= 15f || Input.GetKeyUp("space"))
         {
-            if (isGrounded)
-            {
-                rb.velocity = new Vector2(moveInput * walkSpeed, jumpValue);
-                jumpValue = 0.0f;
-            }
-            canJump = true;
+            OnJump();
         }
+        _animator.SetBool(IsAimingToJump, _isAimingToJump);
+        _animator.SetBool(IsWalking,inputX != 0);
     }
 
-    void ResetJump()
+    private void OnStartAimingToJump()
     {
-        canJump = false;
-        jumpValue = 0.0f;
+        rb.velocity = new Vector2(0.0f, rb.velocity.y);
+        _isAimingToJump = true;
+    }
+
+    private void OnAimingToJump()
+    {
+        jumpValue += 0.02f;
+    }
+
+    private void OnJump()
+    {
+        if (isGrounded)
+        {
+            var inputX = Input.GetAxisRaw("Horizontal");
+            rb.velocity = new Vector2(inputX * walkSpeed, jumpValue);
+            jumpValue = 0.0f;
+            _isAimingToJump = false;
+            canJump = true;
+        }
     }
 }
