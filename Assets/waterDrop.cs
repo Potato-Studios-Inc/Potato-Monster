fcce73fac894ab904c3c02e9e269fd7d05541c4c
Magnus Mølgaard Christensen <magnuschristensen42@gmail.com>
2023-01-17 11:39:24 +0100

    disable waterdrop animation when 'L4 Camera' is not active

--- Assets/waterDrop.cs
+++ Assets/waterDrop.cs
@@ -8,20 +8,26 @@ public class waterDrop : MonoBehaviour
     private bool _playerHit;
     private playerHealth _playerHealth;
     public AudioSource audioSource;
-    
-    
+    public Camera _camera;
+    public Animator animator;
+    public Rigidbody2D player;
+
+
     private void Start()
     {
+        //find 'water-drop-animation' and assign it to anim
+        animator = GameObject.Find("water-drop").GetComponent<Animator>();
+        _camera = GameObject.Find("L4 Camera").GetComponent<Camera>();
         _playerHealth = GameObject.Find("Player").GetComponent<playerHealth>();
         audioSource = GetComponent<AudioSource>();
     }
     
-    void OnTriggerEnter2D(Collider2D col)
+    private void Update()
     {
-        _playerHit = true;
-        _playerHealth.TakeDamage(25);
+        //mute audioSource if the player is not in the 'L4 camera'
+        animator.enabled = _camera.isActiveAndEnabled;
     }
-    
+
     void OnTriggerExit2D(Collider2D col)
     {
         _playerHit = false;
