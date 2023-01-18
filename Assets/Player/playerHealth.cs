5a00a7074a55fe8643ec8c344da3271971773d0f
Sidstebossen <65994287+Sidstebossen@users.noreply.github.com>
2023-01-18 12:11:58 +0100

    gameover implemented
    gameover implemented

--- Assets/Player/playerHealth.cs
+++ Assets/Player/playerHealth.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Player
 {
@@ -7,28 +10,38 @@ namespace Player
     {
         private static readonly int IsDead = Animator.StringToHash("IsDead");
         public int health = 100;
+        public GameObject gameOverMenu;
         private Animator _animator;
-
+        public static event Action OnPlayerDeath;
+      
         private void Start()
         {
             _animator = GetComponent<Animator>();
+            DontDestroyOnLoad(gameOverMenu);
+         
         }
+
+        
 
         private void Update()
         {
+            
             var isDead = _animator.GetBool(IsDead);
             if (!isDead && health <= 0)
             {
                 Die();
+                playerHealth.OnPlayerDeath?.Invoke();
             } else if (isDead && health > 0)
             {
                 Revive();
             }
+
         }
 
         public void TakeDamage(int damage)
         {
             health -= damage;
+          
         }
 
         public void Recover(int heal)
@@ -48,6 +61,43 @@ namespace Player
         void Revive()
         {
             _animator.SetBool(IsDead, false);
+           
+        }
+
+        public void OnEnable()
+        {
+            playerHealth.OnPlayerDeath += EnableGameOverMenu;
+        }
+
+        public void OnDisable()
+        {
+            playerHealth.OnPlayerDeath -= EnableGameOverMenu;
+
+        }
+
+        public void EnableGameOverMenu()
+        {
+            gameOverMenu.SetActive(true);
+        }
+
+        //public void OnEnableStart()
+        //{
+        //    playerHealth.OnPlayerStart += DisableGameOverMenu;
+        //}
+       
+        //public void DisableGameOverMenu()
+        //{
+        //    gameOverMenu.SetActive(false);
+        //}
+
+        public void RestartLevel()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        public void GoToMainMenu()
+        {
+            SceneManager.LoadScene("Menu");
         }
     }
 }
