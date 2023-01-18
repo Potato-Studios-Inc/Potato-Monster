7ac3d78089a37f44756ba8eb672bb486c6fcc492
Magnus Mølgaard Christensen <84289647+MagMC@users.noreply.github.com>
2023-01-18 12:10:59 +0100

    fixed mixer script and setup (#93)

--- Assets/volumescript.cs
+++ Assets/volumescript.cs
@@ -1,15 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class volumescript : MonoBehaviour
 {
-
     public AudioMixer audioMixer;
-
-    public void SetVolume (float Volume)
+    
+    public void SetVolume(float sliderValue)
     {
-        audioMixer.SetFloat("Volume", Volume);
-    } 
+        //set audioMixer volume to slider value
+        audioMixer.SetFloat("Volume", Mathf.Log10(sliderValue) * 20);
+    }
 }
