339e1ebbc2405fa3b4855265eb1a9ae3c5cda2d9
Magnus Mølgaard Christensen <84289647+MagMC@users.noreply.github.com>
2023-01-19 14:15:57 +0100

    added sound to snail (#110)

--- Assets/PatrolEnemy.cs
+++ Assets/PatrolEnemy.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Player;
+using Random = UnityEngine.Random;
 
 public class PatrolEnemy : MonoBehaviour
 {
@@ -11,15 +13,35 @@ public class PatrolEnemy : MonoBehaviour
     int currentPointIndex;
     private SpriteRenderer _renderer;
     public playerHealth player;
+    public AudioSource audioSource;
+    public AudioClip snailSound;
+    public Camera camera;
     bool once;
 
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        snailSound = Resources.Load<AudioClip>("Sounds/snail");
+        audioSource.volume = 0.1f;
+    }
 
     private void Update()
     {
+        //Check if snail is in camera view
+        if (camera.WorldToViewportPoint(transform.position).x is > 0 and < 1 && 
+            camera.WorldToViewportPoint(transform.position).y is > 0 and < 1)
+        {
+            audioSource.mute = false;
+        }
+        else
+        {
+            audioSource.mute = true;
+        }
+
         if (transform.position != patrolPoints[currentPointIndex].position)
         {
-            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[currentPointIndex].position, speed * Time.deltaTime);
-            
+            transform.position = Vector2.MoveTowards(transform.position, 
+                patrolPoints[currentPointIndex].position, speed * Time.deltaTime);
         }
         else
         {
@@ -49,11 +71,16 @@ public class PatrolEnemy : MonoBehaviour
     IEnumerator Wait()
     {
         yield return new WaitForSeconds(waitTime);
-        
+
         if (currentPointIndex + 1 < patrolPoints.Length)
         {
             currentPointIndex++;
             _renderer.flipX = true;
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(snailSound);
+            }
+
         }
         else
         {
